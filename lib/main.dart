d506e0e7f2b9763fe1021c57f3b7701ab9177182
geek957 <akhilralla@Akhils-MacBook-Air.local>
2024-12-01 15:07:50 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'HomePage.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.firstWhere(
+    (camera) => camera.lensDirection == CameraLensDirection.back,
+  );
+
+  runApp(MyApp(camera: firstCamera));
+}
+
+class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  const MyApp({super.key, required this.camera});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(camera: camera),
+    );
+  }
+}
+
