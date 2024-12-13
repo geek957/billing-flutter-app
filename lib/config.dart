9998817d1deba628c4c2fe36966a3831ff27c41d
geek957 <r.akhil.9640@gmail.com>
2024-12-13 15:59:11 +0530

    fix srtting url

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,16 @@
 // config.dart
 class Config {
-  static String apiUrl = 'https://4.240.103.173:5000';
+  static final Config _instance = Config._internal();
+  factory Config() => _instance;
+  Config._internal();
+
+  static String apiUrl = "https://4.240.103.173:5000";
+
+  static void updateApiUrl(String newUrl) {
+    apiUrl = newUrl;
+  }
+
+  // static String getApiUrl() {
+  //   return apiUrl;
+  // }
 }
